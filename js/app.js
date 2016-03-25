900169d7a6b1e1e345fabf066708ed63f72ee0e5
wolfieorama <wachirawamunyi@gmail.com>
2016-03-25 12:40:53 +0300

    added built in directives

--- js/app.js
+++ js/app.js
@@ -2,14 +2,16 @@
 (function(){
   var app = angular.module('store', []);
 
-  app.controller('StoreController', function(){
-    this.product = gem;
-  });
-
   var gem = {
     name: 'Dodecahedron',
     price: 2.95,
     description: 'this is a super gem that doesnt facdein a million years!',
+    // canPurchase: false,
+    soldOut: false,
   }
+
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
 
 })();
