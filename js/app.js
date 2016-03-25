527c66483c1ce999e9dcace301271bb224f88044
wolfieorama <wachirawamunyi@gmail.com>
2016-03-25 12:24:57 +0300

    added controller and directives for the store

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-var app = angular.module('store', []);
+//wrapping your js in a closure is a good habit
+(function(){
+  var app = angular.module('store', []);
+
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: 'this is a super gem that doesnt facdein a million years!',
+  }
+
+})();
