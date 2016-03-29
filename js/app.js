cc643240e463afb3a7075f53af9541b61364b6ef
wolfieorama <wachirawamunyi@gmail.com>
2016-03-29 18:50:43 +0300

    moved logic from view to a new controller

--- js/app.js
+++ js/app.js
@@ -31,4 +31,16 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
 })();
